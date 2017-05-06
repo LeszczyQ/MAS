63e7017056301e48b6d25734f9f0f4dfa3145b7e
LeszczyQ <s12401@pjwstk.edu.pl>
2017-05-06 15:33:35 +0200

    dodana klasa PodzespolUrzadzenie

---
+++ MASTEST/PodzespolUrzadzenie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MASTEST
+{
+
+    [Serializable]
+    class PodzespolUrzadzenie : ObjectPlusPlus
+    {
+        public int Ilosc { get; set; }
+
+        public PodzespolUrzadzenie(int ilosc)
+        {
+            Ilosc = ilosc;
+        }
+    }
+}
