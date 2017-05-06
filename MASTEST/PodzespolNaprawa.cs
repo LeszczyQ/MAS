2cc29d5d53a49029cdde1483f1cb598539dd3c95
LeszczyQ <s12401@pjwstk.edu.pl>
2017-05-06 15:25:26 +0200

    Dodana klasa PodzespolNaprawa

---
+++ MASTEST/PodzespolNaprawa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MASTEST
+{
+    [Serializable]
+    class PodzespolNaprawa : ObjectPlusPlus
+    {
+
+        public int Ilosc { get; set; }
+
+       public PodzespolNaprawa(int ilosc)
+        {
+            Ilosc = ilosc;
+        }
+    }
+}
